936274c6ea207c1e6d80329b58f12909055bd54c
Anou pro <anou.webu@gmail.com>
2026-06-21 21:46:20 +0200

    ✨ feat: add fetch functions for products

---
+++ module-3-challenge-ecommerce-claude-p2/lib/product.ts
@@ -0,0 +1,13 @@
+import { Product } from "@/types";
+
+export async function getProducts(): Promise<Product[]> {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  return data;
+}
+
+export async function getProduct(id: number): Promise<Product> {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await response.json();
+  return data;
+}
