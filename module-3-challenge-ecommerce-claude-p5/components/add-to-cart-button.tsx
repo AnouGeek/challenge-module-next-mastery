8a14f94cc9a552ab9d855b33852f255ae480027f
Anou pro <anou.webu@gmail.com>
2026-06-24 01:25:22 +0200

    ✨ feat: add AddToCartButton with optimistic UI and shadcn Button

---
+++ module-3-challenge-ecommerce-claude-p5/components/add-to-cart-button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { addToCart } from "@/lib/actions";
+import { useOptimistic, useTransition } from "react";
+import { Button } from "./ui/button";
+
+type Props = {
+  productId: number;
+};
+
+export default function AddToCartButton({ productId }: Props) {
+  const [isPending, startTransition] = useTransition();
+
+  const [optimisticText, setOptimisticText] = useOptimistic(
+    "Ajouter au panier",
+    () => "Ajouté",
+  );
+
+  function handleClick() {
+    startTransition(async () => {
+      setOptimisticText("Ajouté");
+      await addToCart(productId);
+    });
+  }
+  return (
+    <Button onClick={handleClick} disabled={isPending} className="w-full mt-4">
+      {optimisticText}
+    </Button>
+  );
+}
